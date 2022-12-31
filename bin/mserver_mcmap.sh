2a72f338ff0918a0342a45b87c2709af612ad7d8
Nate Fitch <nfitch@nfprod.com>
2022-12-31 08:08:33 -0800

    mserver_mcmap.sh now generating world index.

--- bin/mserver_mcmap.sh
+++ bin/mserver_mcmap.sh
@@ -33,10 +33,22 @@ REAL_OUTPUT_DIR=${OUTPUT_DIR}/$NAME
 
 mkdir -p "${REAL_OUTPUT_DIR}/output"
 
-echo "Generating map..."
+echo "Generating map for $NAME..."
 $MCMAP -tile 256 -file $REAL_OUTPUT_DIR/output $WORLDS_DIR/$NAME/world
 
 echo "Finishing up..."
 cp $HTTP_TEMPLATE_DIR/index-map.html $REAL_OUTPUT_DIR/index.html
+
+# Generate the index.html and the world list
+cp $HTTP_TEMPLATE_DIR/index.html $OUTPUT_DIR/index.html
+
+WORLDS_JSON_FILE=$OUTPUT_DIR/worlds.json
+
+WORLD_JSON='{}'
+while read l; do
+    DIR_WORLD_NAME=$(basename $l)
+    WORLD_JSON=$(echo "$WORLD_JSON" | jq ". + { $DIR_WORLD_NAME: {}}")
+done <<<$(ls -d -1 $OUTPUT_DIR/*/ | sort)
+echo "$WORLD_JSON" >$WORLDS_JSON_FILE
 
 echo "Now put $REAL_OUTPUT_DIR behind an http server."
